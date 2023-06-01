3cdde3680cd4b88559be2a9f475194296e373edc
BacktoBasic1 <akashi1234456@gmail.com>
2023-06-01 08:31:33 +0700

    Newest Updated

---
+++ viteProject/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Header, Footer, Blog, Info, Testimonials, Topic} from './Components'
+
+const App = () => {
+  return (
+  <>
+    <Header/>
+    <Topic />
+    <Info />
+    <Blog />
+    <Testimonials />
+    <Footer />
+  </>
+  )
+};
+
+export default App
---
+++ viteProject/src/Assets/index.js
@@ -0,0 +1,24 @@
+
+import daw from './DAW.jpeg'
+import frequencies from './Frequencies.jpeg'
+import music_master from './Music_mastering.jpeg'
+import vocal_process from './Vocal_Processing.jpeg'
+import vocal_training from './Topic_img5.jpeg'
+import bgElement1 from "./bgelement-1.svg"
+import studentIcon from "./student-icon.svg"
+import videoIcon from './Video-icon.svg'
+import bgElement2 from './bgElement2.svg'
+import blog1 from './Thumbnail1.jpg'
+import blog2 from './Thumbnail2.jpg'
+import blog3 from './Thumbnail3.jpg'
+import testimonial1 from './testimonial-img1.png'
+import testimonial2 from './testimonial-img2.png'
+import testimonial3 from './testimonial-img3.png'
+import facebook from './FacebookLogo.svg'
+import instagram from './InstagramLogo.svg'
+import twitter from './TwitterLogo.svg'
+import menuicon from './MenuIcon.svg'
+import exiticon from './closeIcon.svg'
+
+
+export {bgElement1,bgElement2, daw, frequencies, music_master, vocal_process, vocal_training, studentIcon, videoIcon,blog1,blog2,blog3,testimonial1,testimonial2,testimonial3,facebook, instagram, twitter,menuicon,exiticon}
